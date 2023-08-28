adbdcc67737b1bed17f72d6efc5df5fb7213daaa
adrummer1 <andrew@bluestemcreativeco.com>
2023-08-28 11:55:36 -0500

    Added seeds.js with some values.

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,23 @@
-DROP DATABASE IF EXISTS employees_db;
-CREATE DATABASE employees_db;
-
-USE employees_db;
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    dep_name VARCHAR(30),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    dep_name VARCHAR(30) NOT NULL,
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
     dep_id INT
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT
 );
--- server.js
+++ server.js
@@ -21,18 +21,25 @@ inquirer
     .then((answers) => {
         switch (answers.action) {
             case 'View all departments':
+                viewAllDepartments();
                 break;
             case 'View all roles':
+                viewAllRoles();
                 break;
             case 'View all employees':
+                viewAllEmployees();
                 break;
             case 'Add a department':
+                addDepartment();
                 break;
             case 'Add a role':
+                addRole();
                 break;
             case 'Add an employee':
+                addEmployee();
                 break;
             case 'Update an employee role':
+                updateEmployeeRole();
                 break;
             default:
                 console.log('Invalid action.');    
@@ -103,5 +110,5 @@ inquirer
     }
 
     function updateEmployeeRole() {
-        
+
     }
