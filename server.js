93c56f0590b5b350b5371de3e68bce8b961626c6
adrummer1 <andrew@bluestemcreativeco.com>
2023-08-29 15:20:07 -0500

    Updated all functions in server.js to correct db mapping bugs.

--- server.js
+++ server.js
@@ -49,32 +49,39 @@ const promptUser = () => {
 };
 
 // Functions for supplying database content
-const viewAllDepartments = () => {
-  const sql = `SELECT department.id AS id, department.dep_name AS department FROM department`;
-  connection.promise().query(sql, (err, res) => {
-    if (err) throw error;
+const viewAllDepartments = async () => {
+  try {
+    const sql = `SELECT department.id AS id, department.dep_name AS department FROM department`;
+    const [rows, fields] = await connection.promise().query(sql);
     promptUser();
-  });
+  } catch (err) {
+    throw err;
+  }
 };
 
-const viewAllRoles = () => {
-  const sql = `SELECT role.id, role.title, department.dep_name AS department 
-                    FROM role INNER JOIN department ON role.department.id = department.id`;
-  connection.promise().query(sql, (err, res) => {
-    if (err) throw error;
+const viewAllRoles = async () => {
+  try {
+    const sql = `SELECT role.id AS id, role.title AS title, role.salary AS salary, department.dep_name AS department 
+              FROM role JOIN department ON role.dep_id = department.id`;
+    const [rows, fields] = await connection.promise().query(sql);
     promptUser();
-  });
+  } catch (err) {
+    throw err;
+  }
 };
 
-const viewAllEmployees = () => {
-  const sql = `SELECT employee.id, employee.first_name, emplyee.last_name,
-                    role.title, department.dep_name AS department, role.salary 
-                    FROM employee, role, department WHERE departement.id = role.department.id 
-                    AND role.id = employee.role_id ORDER BY emplyee.id ASC`;
-  connection.promise().query(sql, (err, res) => {
-    if (err) throw error;
+const viewAllEmployees = async () => {
+  try {
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name,
+              role.title, department.dep_name AS department, role.salary 
+              FROM employee, role, department WHERE department.id = role.dep_id 
+              AND role.id = employee.role_id ORDER BY employee.id ASC`;
+    const [rows, fields] = await connection.promise().query(sql);
+    console.table(rows);
     promptUser();
-  });
+  } catch (err) {
+    throw err;
+  }
 };
 
 const addDepartment = () => {
@@ -88,8 +95,8 @@ const addDepartment = () => {
     ])
     .then((answer) => {
       let sql = `INSERT INTO department (dep_name) VALUES (?)`;
-      connection.query(sql, answer.newDepartment, (err, res) => {
-        if (err) throw error;
+      connection.query(sql, answer.departmentName, (err, res) => {
+        if (err) throw err;
         viewAllDepartments();
       });
     });
@@ -111,15 +118,19 @@ const addRole = () => {
       {
         type: "input",
         name: "department",
-        message: "What department will this new role be in?",
+        message: "What is the department ID that this new role be in?",
       },
     ])
     .then((answer) => {
-      let sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
-      connection.query(sql, answer.newRole, (err, res) => {
-        if (err) throw error;
-        viewAllRoles();
-      });
+      let sql = `INSERT INTO role (title, salary, dep_id) VALUES (?, ?, ?)`;
+      connection.query(
+        sql,
+        [answer.newRole, answer.salary, answer.department],
+        (err, res) => {
+          if (err) throw err;
+          viewAllRoles();
+        }
+      );
     });
 };
 
@@ -139,77 +150,80 @@ const addEmployee = () => {
       {
         type: "input",
         name: "role",
-        message: "What is the role of the new employee?",
+        message: "What is the role ID for the role of the new employee?",
       },
       {
         type: "input",
         name: "manager",
-        message: "Who is the manager of the new employee?",
+        message: "Who is the manager ID for the manager of the new employee?",
       },
     ])
     .then((answer) => {
-      let sql = `INSER INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
-      connection.query(sql, answer.newEmployee, (err, res) => {
-        if (err) throw error;
-        viewAllEmployees;
-      });
+      let sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+      connection.query(
+        sql,
+        [answer.firstName, answer.lastName, answer.role, answer.manager],
+        (err, res) => {
+          if (err) throw error;
+          viewAllEmployees();
+        }
+      );
     });
 };
 
-const updateEmployeeRole = () => {
-  let sql = `SELECT employee.id, employee.first_name, employee.last_name, 
+const updateEmployeeRole = async () => {
+  try {
+    let sql = `SELECT employee.id, employee.first_name, employee.last_name, 
                     role.id AS "role_id" FROM employee, role, department WHERE
                     department.id AND role.id = employee.role_id`;
-  connection.promise().query(sql, (err, res) => {
-    if (err) throw error;
-    let employeeNames = [];
-    response.forEach((employee) => {
-      employeeNames.push(`${employee.first_name} ${employee.last_name}`);
-    });
-    let sql = `SELECT role.id, role.title FROM role`;
-    connection.promise().query(sql, (err, res) => {
-      if (error) throw error;
-      let rolesArray = [];
-      response.forEach((role) => {
-        rolesArray.push(role.title);
-      });
-      inquierer
-        .prompt([
-          {
-            type: "list",
-            name: "chosenEmployee",
-            message: "Which employee has a new role?",
-            choices: employeeNames,
-          },
-          {
-            type: "list",
-            name: "chosenRole",
-            message: "What is their new role?",
-            choices: rolesArray,
-          },
-        ])
-        .then((answer) => {
-          let newTitleId, employeeId;
-          response.forEach((role) => {
-            if (answer.chosenRole === role.title) {
-              newTitleId = role.id;
-            }
-          });
+    const employees = await connection.promise().query(sql);
+    let employeeNames = employees.map(
+      (employee) => `${employee.first_name} ${employee.last_name}`
+    );
 
-          response.forEach((employee) => {
-            if (
-              answer.chosenEmployee ===
-              `${employee.first_name} ${employee.last_name}`
-            ) {
-              employeeId = employee.id;
-            }
-          });
-          let updateSql = `UPDATE employee SET employee.role_id = ? WHERE employee.id = ?`;
-          connection.query(updateSql, [newTitleId, employeeId], (err) => {
-            if (err) throw error;
-            promptUser();
-          });
-        });
+    let roleSql = `SELECT role.id, role.title FROM role`;
+    const roles = await connection.promise().query(roleSql);
+    let rolesArray = roles.map((role) => role.title);
+
+    const employeeChoices = employeeNames.map((name) => ({ name}));
+    const roleChoices = rolesArray.map((title) => ({ name: title }));
+
+    const answer = await inquirer.prompt([
+      {
+        type: "list",
+        name: "chosenEmployee",
+        message: "Which employee has a new role?",
+        choices: employeeChoices,
+      },
+      {
+        type: "list",
+        name: "chosenRole",
+        message: "What is their new role?",
+        choices: roleChoices,
+      },
+    ]);
+
+    let newTitleId, employeeId;
+    roles.forEach((role) => {
+      if (answer.chosenRole === role.title) {
+        newTitleId = role.id;
+      }
     });
-  });
+
+    employees.forEach((employee) => {
+      if (`${employee.first_name} ${employee.last_name}`) {
+        employeeId = employee.id;
+      }
+    });
+
+    let updateSql = `UPDATE employee SET employee.role_id = ? WHERE employee.id = ?`;
+    await connection.promise().query(updateSql, [newTitleId, employeeId]);
+
+    promptUser();
+  } catch (error) {
+    throw error;
+  }
 };
+    
+promptUser();
+
