fd14b61c20facb999894f04ee7546e6b63b03f97
adrummer1 <andrew@bluestemcreativeco.com>
2023-09-08 13:22:13 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'LMP10012004',
+    password: 'XXXXXX',
     database: 'employees'
 });
 
